b3ebd6b5a319a3c106adade2b0eda7f4b6529047
Paulo Roberto Ramos de Andrade <paulo.roberto@randrade.net>
2020-07-29 16:20:39 -0300

    Adding XmlDoc

--- DateTimeUtils/Interefaces/IDateTimeUtils.cs
+++ DateTimeUtils/Interefaces/IDateTimeUtils.cs
@@ -4,10 +4,34 @@
 
     public interface IDateTimeUtils
     {
+        /// <summary>
+        /// Retrieves <see cref="DateTime.MinValue"/> value.
+        /// </summary>
+        /// <returns>DateTime Minimum Value</returns>
         public DateTime MinValue();
+
+        /// <summary>
+        /// Retrieves <see cref="DateTime.MaxValue"/> value.
+        /// </summary>
+        /// <returns>DateTime Maximum Value</returns>
         public DateTime MaxValue();
+
+        /// <summary>
+        /// Retrieves <see cref="DateTime.Now"/> value.
+        /// </summary>
+        /// <returns>DateTime Now Value</returns>
         public DateTime Now();
+
+        /// <summary>
+        /// Retrieves <see cref="DateTime.UtcNow"/> value.
+        /// </summary>
+        /// <returns>DateTime UtcNow Value</returns>
         public DateTime UtcNow();
+
+        /// <summary>
+        /// Retrieves <see cref="DateTime.Today"/> value.
+        /// </summary>
+        /// <returns>DateTime Today Value</returns>
         public DateTime Today();
     }
 }
--- DateTimeUtils/Services/DateTimeUtils.cs
+++ DateTimeUtils/Services/DateTimeUtils.cs
@@ -5,10 +5,34 @@
 
     public class DateTimeUtils : IDateTimeUtils
     {
+        /// <summary>
+        /// Retrieves <see cref="DateTime.MinValue"/> value.
+        /// </summary>
+        /// <returns>DateTime Minimum Value</returns>
         public DateTime MinValue() => DateTime.MinValue;
+
+        /// <summary>
+        /// Retrieves <see cref="DateTime.MaxValue"/> value.
+        /// </summary>
+        /// <returns>DateTime Maximum Value</returns>
         public DateTime MaxValue() => DateTime.MaxValue;
+
+        /// <summary>
+        /// Retrieves <see cref="DateTime.MinValue"/> value.
+        /// </summary>
+        /// <returns>DateTime Minimum Value</returns>
         public DateTime Now() => DateTime.Now;
+
+        /// <summary>
+        /// Retrieves <see cref="DateTime.MinValue"/> value.
+        /// </summary>
+        /// <returns>DateTime Minimum Value</returns>
         public DateTime UtcNow() => DateTime.UtcNow;
+
+        /// <summary>
+        /// Retrieves <see cref="DateTime.MinValue"/> value.
+        /// </summary>
+        /// <returns>DateTime Minimum Value</returns>
         public DateTime Today() => DateTime.Today;
     }
 }
